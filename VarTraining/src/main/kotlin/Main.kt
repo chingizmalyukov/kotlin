a099fd0b63ddde0e4b7823c1270105c22411596d
chingizmalyukov <killerban4@gmail.com>
2023-08-22 14:26:36 +0300

    finish

---
+++ VarTraining/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    val firstName : String = "Chingiz"
+    val lastName : String = "Malyukov"
+    var height : Double = 1.83
+    val weight : Float = 95.0F
+
+    var isChild : Boolean
+
+    println("Hi my name is $firstName my last name is $lastName")
+
+
+
+
+}
